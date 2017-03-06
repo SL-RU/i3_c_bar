02069e5095f7d6a0fb6bb68570434c72f32025fe
Alexander Lutsai <s.lyra@ya.ru>
2017-03-07 02:20:40 +0300

    cmake

---
+++ project.el
@@ -0,0 +1,6 @@
+(ede-add-project-to-global-list
+ (ede-compdb-project "MakiseSDLTest"
+		     :file "/home/lyra/.i3/i3_c_bar/CMakeLists.txt"
+		     :build-command "make"
+		     :compdb-file "build/compile_commands.json")
+)
